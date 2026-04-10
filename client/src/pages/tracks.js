c585dbc76fc43ea55ba3f2fb7bec0ed9122ac480
Tim Oehler <timothy.oehler@thrivent.com>
2026-04-10 12:46:38 -0500

    Apollo Client Setup

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,5 +1,22 @@
-import React from 'react';
-import { Layout } from '../components';
+import React from "react";
+import { Layout } from "../components";
+import { gql } from "@apollo/client";
+
+const TRACKS = gql`
+  query GetTracks {
+    tracksForHome {
+      id
+      title
+      author {
+        name
+        photo
+      }
+      thumbnail
+      length
+      modulesCount
+    }
+  }
+`;
 
 /**
  * Tracks Page is the Catstronauts home page.
